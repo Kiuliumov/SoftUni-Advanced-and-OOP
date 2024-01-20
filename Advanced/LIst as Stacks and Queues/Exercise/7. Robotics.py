fb498422e211950ce3238f92d09152da9f02741d
Kiuliumov <miti.bania@gmail.com>
2024-01-20 23:27:44 +0200

    Advanced

--- Advanced/LIst as Stacks and Queues/Exercise/7. Robotics.py
+++ Advanced/LIst as Stacks and Queues/Exercise/7. Robotics.py
@@ -1,30 +1,60 @@
 from collections import deque
-from datetime import datetime, timedelta
 
-robots = {}
+robots_list = list(input().split(";"))
+free_robots_dict = {}
+working_robots_dict = {}
 
-for r in input().split(';'):
-    name,time_needed = r.split('-')
-    robots[name] = [time_needed,0]
-factory_time = datetime.strptime(input())
-products = deque()
+product_deque = deque()
+working_deque = deque()
+
+for robs in robots_list:
+    robot_data = robs.split("-")
+    working_robots_dict[robot_data[0]] = int(robot_data[1])
+    free_robots_dict[robot_data[0]] = -1
+
+starting_time = input().split(":")
+hours = int(starting_time[0])
+minutes = int(starting_time[1])
+seconds = int(starting_time[2])
 
 while True:
     product = input()
-    if product == 'End':
+    if product == "End":
         break
-    products.append(product)
+    product_deque.append(product)
 
-while products:
-    factory_time += timedelta(0,1)
-    free_robots = []
-    product = products.popleft()
-    for name,value in robots.items():
-        if value[1] != 0:
-            robots[name][1] -= 1
+while product_deque or working_deque:
+    seconds += 1
+    if product_deque:
+        working_deque.append(product_deque.popleft())
+    if seconds == 60:
+        seconds = 0
+        minutes += 1
+    if minutes == 60:
+        minutes = 0
+        hours += 1
+    if hours == 24:
+        hours = 0
+
+    for rob in free_robots_dict:
+        if working_deque:
+            if free_robots_dict[rob] == -1:
+                free_robots_dict[rob] = 0
+                print(f"{rob} - {working_deque.popleft()} [{hours:02d}:{minutes:02d}:{seconds:02d}]")
+                continue
+            elif free_robots_dict[rob] > -1:
+                free_robots_dict[rob] += 1
+                if free_robots_dict[rob] >= working_robots_dict[rob]:
+                    free_robots_dict[rob] = -1
+                    if working_deque:
+                        free_robots_dict[rob] = 0
+                        print(f"{rob} - {working_deque.popleft()} [{hours:02d}:{minutes:02d}:{seconds:02d}]")
+                        continue
         else:
-            free_robots.append([name,value])
-    if not free_robots:
-        products.append(product)
-        continue
-    robots_name,data = free_robots[0]
+            if free_robots_dict[rob] > -1:
+                free_robots_dict[rob] += 1
+                if free_robots_dict[rob] >= working_robots_dict[rob]:
+                    free_robots_dict[rob] = -1
+    else:
+        if working_deque:
+            product_deque.append(working_deque.popleft())
