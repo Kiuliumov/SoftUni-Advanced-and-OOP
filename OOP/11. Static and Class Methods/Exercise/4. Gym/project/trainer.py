03f8ae7b08969309b0e0c565db7e2b3d6486d0f7
Kiuliumov <miti.bania@gmail.com>
2024-03-02 16:29:31 +0200

    Advanced

--- OOP/11. Static and Class Methods/Exercise/4. Gym/project/trainer.py
+++ OOP/11. Static and Class Methods/Exercise/4. Gym/project/trainer.py
@@ -1,15 +1,16 @@
 class Trainer:
-    ID = 1
+    id = 1
 
     def __init__(self, name: str):
         self.name = name
-        self.id = Trainer.ID
-        Trainer.ID += 1
+        self.id = Trainer.id
+        Trainer.id += 1
 
     @staticmethod
     def get_next_id():
-        return Trainer.ID
+        return Trainer.id
 
-    def __str__(self):
+    def __repr__(self):
         return 'Trainer <{}> {}'.format(self.id, self.name)
+
 
