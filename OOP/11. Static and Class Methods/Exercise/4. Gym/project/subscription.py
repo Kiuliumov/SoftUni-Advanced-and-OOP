3fd613a45e26e89f60f1a6aa2625c2d703a4c216
Kiuliumov <miti.bania@gmail.com>
2024-03-02 16:29:15 +0200

    Advanced

--- OOP/11. Static and Class Methods/Exercise/4. Gym/project/subscription.py
+++ OOP/11. Static and Class Methods/Exercise/4. Gym/project/subscription.py
@@ -1,17 +1,17 @@
 class Subscription:
-    ID = 1
+    id = 1
 
     def __init__(self, date: str, customer_id: int, trainer_id: int, exercise_id: int) -> None:
         self.date = date
         self.customer_id = customer_id
         self.trainer_id = trainer_id
         self.exercise_id = exercise_id
-        self.id = Subscription.ID
-        Subscription.ID += 1
+        self.id = Subscription.id
+        Subscription.id += 1
 
     @staticmethod
     def get_next_id() -> int:
-        return Subscription.ID
+        return Subscription.id
 
     def __repr__(self) -> str:
         return 'Subscription <{}> on {}'.format(self.id, self.date)
