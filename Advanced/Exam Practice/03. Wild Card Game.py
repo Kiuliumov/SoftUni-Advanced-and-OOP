5cfa4b2362f72026884b725fca3d91ed6c476678
Kiuliumov <miti.bania@gmail.com>
2024-04-21 18:56:30 +0300

    Advanced

--- Advanced/Exam Practice/03. Wild Card Game.py
+++ Advanced/Exam Practice/03. Wild Card Game.py
@@ -14,22 +14,16 @@ def draw_cards(*args, **kwargs):
         elif card_type == "spell":
             spell_cards[card_name] = card_type
 
-    sorted_monster_cards = sorted(monster_cards.keys(), reverse=True)
-
-    sorted_spell_cards = sorted(spell_cards.keys())
-
     result = ""
 
-    if sorted_monster_cards:
+    if monster_cards:
         result += "Monster cards:\n"
-        for card_name in sorted_monster_cards:
-            result += f"{card_name} - {monster_cards[card_name]}\n"
+        for card_name in sorted(monster_cards.keys(), reverse=True):
+            result += f"  ***{card_name}\n"
 
-    if sorted_spell_cards:
+    if spell_cards:
         result += "Spell cards:\n"
-        for card_name in sorted_spell_cards:
-            result += f"{card_name} - {spell_cards[card_name]}\n"
+        for card_name in sorted(spell_cards.keys()):
+            result += f"  $$${card_name}\n"
 
     return result.strip()
-
-print(draw_cards(("cyber dragon", "monster"), freeze="spell",))
