14bab17d0855391dbd0f8750e566d7903d1f5699
Kiuliumov <miti.bania@gmail.com>
2024-06-28 07:07:57 +0300

    OOP Exercises

--- OOP/19. Decorators/Exercise/9. Computer Store/project/computer_types/desktop_computer.py
+++ OOP/19. Decorators/Exercise/9. Computer Store/project/computer_types/desktop_computer.py
@@ -2,7 +2,7 @@ from project.computer_types.computer import Computer
 
 
 class DesktopComputer(Computer):
-    AVAILABLE_PROCESSORS = {'AMD Ryzen 7 5700G': 500, 'Intel Core i5-12600K': 600, 'Apple M1 Max': 1800}
+    AVAILABLE_PROCESSORS = {'AMD Ryzen 7. Spoopify 5700G': 500, 'Intel Core i5-12600K': 600, 'Apple M1 Max': 1800}
     MAX_RAM = 128
 
     def __init__(self, manufacturer, model):
