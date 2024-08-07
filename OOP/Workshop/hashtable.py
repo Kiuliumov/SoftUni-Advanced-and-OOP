bbbfde70faf9f0d51743999e8fe00fa0fd97c0e1
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2024-08-07 18:24:57 +0300

    OOP

---
+++ OOP/Workshop/hashtable.py
@@ -0,0 +1,11 @@
+class HashTable:
+    INITIAL_CAPACITY = 4
+
+    def __init__(self):
+        self.capacity = self.INITIAL_CAPACITY
+        self.size = 0
+        self.array = [None] * self.capacity
+
+    
+
+
